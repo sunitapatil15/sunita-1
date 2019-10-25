b4bcfe3781859aafad41aa1dd45d042b5ef129fa
Sunita Patil <sunita.patil0105@gmail.com>
2019-10-25 12:47:23 +0530

    Initial commit

---
+++ 1D_ADDITION.asm
@@ -0,0 +1,65 @@
+  .MODEL SMALL
+  .DATA
+     MSG1 DB 10,13,"ENTER VALUE1:$" 
+     MSG2 DB 10,13,"ENTER VALUE2:$"    
+     MSG3 DB 10,13,"ADDITION VALU:$"
+     
+     .CODE
+     MOV AX,@DATA
+     MOV DS,AX
+     
+     MOV AH,09H
+     LEA DX,MSG1
+     INT 21H    
+     
+     
+     MOV AH,01H
+     INT 21H
+     
+     CMP AL,39H    ;COMPARE VALUE OF AL WITH 39H
+     JBE X1          ;CONDITIONAL JUMP TO GIVEN LABEL NAME BELOW OR EQUAL WHICH CAN BE KNOWN FROM PREVIOUS COMPARISION INSTRUCTION
+     
+     SUB AL,07H
+     X1:SUB AL,30H 
+     
+     MOV BH,AL
+     
+     
+     MOV AH,09H
+     LEA DX,MSG2
+     INT 21H 
+     
+     
+     MOV AH,01H
+     INT 21H
+     
+     CMP AL,39H    ;COMPARE VALUE OF AL WITH 39H
+     JBE X2          ;CONDITIONAL JUMP TO GIVEN LABEL NAME BELOW OR EQUAL WHICH CAN BE KNOWN FROM PREVIOUS COMPARISION INSTRUCTION
+     
+     SUB AL,07H
+     X2:SUB AL,30H 
+     
+     MOV BL,AL 
+     
+     ADD BL,BH
+     
+     MOV AH,09H
+     LEA DX,MSG3
+     INT 21H 
+      
+     
+     CMP BL,09
+     JBE X3
+     
+     ADD BL,07H
+     X3:ADD BL,30H
+     
+     
+     
+     MOV AH,02H 
+     MOV DL,BL
+     INT 21H
+     
+     MOV AH,4CH
+     INT 21H
+     END
